b6b1ee7c9fc4b3dd4c9ff415ad24cd2db893a531
Aditya Shah <ashah8989@gmail.com>
2018-10-13 09:02:53 -0400

    southpark clicky game using react framework

---
+++ README.md
@@ -0,0 +1,10 @@
+# Clicky-Game
+## Overview
+
+This game is a memory game built with React. All of the characters are from South Park and the goal is not to click on any character more than once. If you select a chracter for second time the game will restart and if all characters are selected once, the selected characters will reset.
+
+## Deployed on Github Pages
+
+[Play](https://neginnami.github.io/Clicky-Game)
+
+REACT APP
---
+++ src/components/footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<footer className='page-footer blue'>
+			<div className='container'>
+				<div className='row'>
+					<div className='col l7 s12'>
+					</div>
+					<div className='col l4 offset-l1 s12'>
+						<div className='row'>	
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className='footer-copyright'>
+				<div className='container'>
+					<span>© 2018 Aditya Shah</span>
+				</div>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
